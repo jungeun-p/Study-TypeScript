e742489294e857eb2b432f6a87108a80927ef17d
jungeun-p <mrk7753@naver.com>
2021-11-04 16:43:24 +0900

    [ create ] sample.js, sample.ts
    - js, ts의 차이점과 js에서 type을 보완하는 방법에 대한 실습.

--- why-ts/app.js
+++ why-ts/app.js
@@ -9,13 +9,40 @@ var address = document.querySelector("#address");
 // user data
 var user = {};
 
+/**
+ * @typedef {object} Address
+ * @property {string} street
+ * @property {string} city
+ *
+ */
+
+/**
+ * @typedef {object} User // type에 대한 type 객체의 property 설정
+ * @property {string} name
+ * @property {string} email
+ * @property {Address} address
+ */
+
+/**
+ * @returns {Promise<User>} //generic
+ */
+
+function fetchUser() {
+  return axios.get(url);
+}
+
+fetchUser().then(function (response) {});
+
 function startApp() {
-  axios
-    .get(url)
+  fetchUser()
     .then(function (response) {
-      console.log(response);
+      // console.log(response);
       user = response.data;
       // TODO: 이름, 이메일, 주소 표시하기
+      console.log(user);
+      username.innerText = user[0].name;
+      email.innerText = user[0].email;
+      address.innerText = user[0].address;
     })
     .catch(function (error) {
       console.log(error);
---
+++ why-ts/sample.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * @param {number} a
+ * @param {number} b
+ */
+
+// @ts-check
+
+function sum(a, b) {
+  return a + b;
+}
+
+sum(10, "20");
