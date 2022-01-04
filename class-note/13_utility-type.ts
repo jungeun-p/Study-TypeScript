693a985c6ddea33f7acd853912910de6a334d76c
jungeun-p <mrk7753@naver.com>
2022-01-05 00:00:45 +0900

    [ modify ] Partial 유틸리티 타입 구현
    - key값에 접근 후 반복문, generic 타입 순서대로 Partial 유틸리티 타입
      구현 처리.

--- class-note/13_utility-type.ts
+++ class-note/13_utility-type.ts
@@ -34,9 +34,47 @@ interface UpdateProduct{
     stock: number;
 }
 
-type updateproduct = Partial<Product>
-
+type updateProduct = Partial<Product>
 // 3. 특정 상품 정보를 업데이트하는 함수 
-function updateProductItem(productItem: updateproduct){
+function updateProductItem(productItem: updateProduct){
 
+}
+
+// 4. 유틸리티 타입 구현하기 - Partial
+interface UserProfile {
+    username: string;
+    email: string;
+    profilePhotoUrl: string; 
+}
+
+type userProfileUpdate = Partial<UserProfile>
+
+// interface UserProfileUpdate {
+//     username?: string;
+//     email?: string;
+//     profilePhotoUrl?: string; 
+// }
+
+// // #1
+// interface UserProfileUpdate {
+//     username?: UserProfile['username'];
+//     email?: UserProfile['email'];
+//     profilePhotoUrl?: UserProfile['profilePhotoUrl'];
+// }
+
+// // #2
+// type UserProfileUpdate = {
+//     [p in 'username' | 'email' | 'profilePhotoUrl']?: UserProfile[p]
+//     // 반복문을 이용한 유틸리티 타입 구현
+// }
+
+// // #3
+// type UserProfileUpdate = {
+//     [p in keyof UserProfile]?: UserProfile[p]
+//     // UserProfile 국한되어 있는 형태. 
+// }
+
+// #4 타입을 넘겨 받을 수 있는 형태. 
+type Subset<T> = {
+    [p in keyof T]?: T[p]
 }
