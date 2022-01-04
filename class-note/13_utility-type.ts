610f6138ad32793e6824550a61eff7ea5a99b0fc
jungeun-p <mrk7753@naver.com>
2022-01-04 23:37:40 +0900

    [ modify ] Partial을 이용한 실습
    - Partial을 이용하여 해당 속성 optional 처리 실습

--- class-note/13_utility-type.ts
+++ class-note/13_utility-type.ts
@@ -6,7 +6,7 @@ interface Product {
     stock: number;
 }
 
-// 상품 목록을 받아오기 위한 API 함수
+// 1. 상품 목록을 받아오기 위한 API 함수
 function fetchProducts(): Promise<Product[]> {
     
 }
@@ -20,8 +20,23 @@ function fetchProducts(): Promise<Product[]> {
 type ShoppingItem = Pick<Product, 'id' | 'name' | 'price'>
 type ShoppingItem_two = Omit<Product, 'stock'>
 
-// 클릭 후 상세 정보 페이지 API 함수
+// 2. 클릭 후 상세 정보 페이지 API 함수
 function displayProductDetail(shoppingItem: ShoppingItem) {
 
 }
 
+
+interface UpdateProduct{
+    id: number;
+    name: string;
+    price: number;
+    brand: string;
+    stock: number;
+}
+
+type updateproduct = Partial<Product>
+
+// 3. 특정 상품 정보를 업데이트하는 함수 
+function updateProductItem(productItem: updateproduct){
+
+}
