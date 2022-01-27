d6cd2dfd6014b335a9fac07238fb42e47bc2a1b6
jungeun-p <mrk7753@naver.com>
2022-01-27 22:29:26 +0900

    [ modify ] 04. 점진적인 타입 적용(2)
    - enum을 이용한 params 정의
    - DOM 함수 관련 타입 오류 분석 및 적용 : 타입 단언을 이용하여 호환
      가능한 element 타입 정의 처리

--- project/src/app.ts
+++ project/src/app.ts
@@ -1,23 +1,24 @@
 // utils
-function $(selector: any) {
+function $(selector: string) {
   return document.querySelector(selector);
 }
-function getUnixTimestamp(date: any) {
+function getUnixTimestamp(date: Date) {
   return new Date(date).getTime();
 }
 
 // DOM
-const confirmedTotal = $(".confirmed-total");
-const deathsTotal = $(".deaths");
-const recoveredTotal = $(".recovered");
-const lastUpdatedTime = $(".last-updated-time");
+var a: Element | HTMLElement | HTMLParagraphElement
+const confirmedTotal = $(".confirmed-total") as HTMLSpanElement;
+const deathsTotal = $(".deaths") as HTMLParagraphElement;
+const recoveredTotal = $(".recovered") as HTMLParagraphElement;
+const lastUpdatedTime = $(".last-updated-time") as HTMLParagraphElement;
 const rankList = $(".rank-list");
 const deathsList = $(".deaths-list");
 const recoveredList = $(".recovered-list");
 const deathSpinner = createSpinnerElement("deaths-spinner");
 const recoveredSpinner = createSpinnerElement("recovered-spinner");
 
-function createSpinnerElement(id: any) {
+function createSpinnerElement(id: any) { // id값에 들어갈 문자열
   const wrapperDiv = document.createElement("div");
   wrapperDiv.setAttribute("id", id);
   wrapperDiv.setAttribute(
@@ -42,10 +43,16 @@ function fetchCovidSummary() {
   return axios.get(url);
 }
 
-function fetchCountryInfo(countryCode : any, status: any) {
-  // params: confirmed, recovered, deaths
+enum CovidStatus{
+  Confimred = 'confirmed',
+  Recovered = 'recovered',
+  Deaths = 'Deaths'
+}
+
+function fetchCountryInfo(countryCode : string, status: CovidStatus) {
+  // status params: confirmed, recovered, deaths
   const url = `https://api.covid19api.com/country/${countryCode}/status/${status}`;
-  return axios.get(url);
+  return axios.get(url); 
 }
 
 // methods
@@ -77,14 +84,14 @@ async function handleListClick(event: any) {
   clearRecoveredList();
   startLoadingAnimation();
   isDeathLoading = true;
-  const { data: deathResponse } = await fetchCountryInfo(selectedId, "deaths");
+  const { data: deathResponse } = await fetchCountryInfo(selectedId, CovidStatus.Deaths);
   const { data: recoveredResponse } = await fetchCountryInfo(
     selectedId,
-    "recovered"
+    CovidStatus.Recovered
   );
   const { data: confirmedResponse } = await fetchCountryInfo(
     selectedId,
-    "confirmed"
+    CovidStatus.Confimred
   );
   endLoadingAnimation();
   setDeathsList(deathResponse);
